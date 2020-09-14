d2484445c76ab8fad60cf5426a7ec1d674736d97
abdelrhman997 <a.ghg997@gmail.com>
2020-09-14 17:36:18 +0200

    last update

--- SHMS/app/src/main/java/com/example/shms/Login.java
+++ SHMS/app/src/main/java/com/example/shms/Login.java
@@ -2,13 +2,26 @@ package com.example.shms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
+    TextView Signupbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Signupbtn=findViewById(R.id.textView3);
+        Signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),sign_up.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
